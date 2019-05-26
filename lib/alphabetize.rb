43f71a5ed7dd9e7fcda82279b6ca64410b9fa19c
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-26 15:57:04 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
-def alphabetize(arr)
-  # code here
+
+
+ def alphabetize(sentences_array)
+  sentences_array.sort_by do |phrase|
+    phrase.split(“”).map do |character|
+      ESPERANTO_ALPHABET.index(character)
+	  end
+	end
 end
